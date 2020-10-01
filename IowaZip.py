d215ef974433ef1eac4a37def04e84f4bd3c6fb7
maxbarshay <mbarshay@calpoly.edu>
2020-09-30 17:32:51 -0700

    working zip codes

--- IowaZip.py
+++ IowaZip.py
@@ -32,21 +32,53 @@ for code in soup.find_all('tr'):
         
 
 res = res[:-1]
-print(res)
+# print(res)
 
 
-for i in range(len(res)):
+for i in range(0, len(res), 3):
 
 
-    if i % 3 == 2:
 
-        prev = res[i-1][2]
-        code = re.search(r"\d+", prev).group()
-        curr = res[i][1]
-        pop = re.search(r"\d+,?\d+", curr).group()
+    val = res[i][-3]
+
+    #print(val)
+    if val == "TIE":
+        #print("do stuff")
+
+        and_count = 0
+        rel = res[i+1]
+
+        to_add = []
+
+        for j in range(len(rel)):
+
+
+            if re.search(r'f=\"(\d+)', rel[j]) is not None:
+
+                to_add.append(re.search(r'f=\"(\d+)', rel[j]).group(1))
+
+        popspot = res[i+2][1]
+        pop = re.search(r"\d+,?\d+", popspot).group()
+
+        for zipcode in to_add:
+
+            zipPops[int(zipcode)] = pop
+
+
+    else:
+        
+    # if i % 3 == 2:
+        codespot = res[i+1][2]
+        code = re.search(r"\d+", codespot).group()
+        popspot = res[i+2][1]
+        pop = re.search(r"\d+,?\d+", popspot).group()
         zipPops[int(code)] = pop
 
 
-print(zipPops)
 
+#
+print(zipPops)
+print(len(zipPops))
 # print(soup)
+
+
